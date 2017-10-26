7da5c52f6d09b2d8c19cb656204f4bb7a334f87c
Jan Spidlen <jan.spidlen@airbnb.com>
2017-10-25 20:39:29 -0700

    Adding floating action button + circular reveal animation.

--- app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -1,12 +1,21 @@
 package com.codepath.android.lollipopexercise.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
+import android.transition.Transition;
+import android.transition.TransitionListenerAdapter;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,6 +33,10 @@ public class DetailsActivity extends AppCompatActivity {
     private TextView tvPhone;
     private View vPalette;
 
+    private FloatingActionButton fab;
+    private Transition.TransitionListener mEnterTransitionListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +46,9 @@ public class DetailsActivity extends AppCompatActivity {
         tvName = (TextView) findViewById(R.id.tvName);
         tvPhone = (TextView) findViewById(R.id.tvPhone);
         vPalette = findViewById(R.id.vPalette);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setVisibility(View.INVISIBLE);
+
 
         // Extract contact from bundle
         mContact = (Contact)getIntent().getExtras().getSerializable(EXTRA_CONTACT);
@@ -42,6 +58,15 @@ public class DetailsActivity extends AppCompatActivity {
         tvName.setText(mContact.getName());
         tvPhone.setText(mContact.getNumber());
 
+        mEnterTransitionListener = new TransitionListenerAdapter() {
+            @Override
+            public void onTransitionEnd(@NonNull Transition transition) {
+                enterReveal();
+            }
+        };
+
+        getWindow().getEnterTransition().addListener(mEnterTransitionListener);
+        setupFab();
 
         Target target = new Target() {
             // Fires when Picasso finishes loading the bitmap for the target
@@ -72,6 +97,19 @@ public class DetailsActivity extends AppCompatActivity {
 
         ivProfile.setTag(target);
         Picasso.with(this).load(mContact.getThumbnailDrawable()).into(target);
+
+    }
+
+    private void setupFab() {
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:" + mContact.getNumber();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -79,10 +117,70 @@ public class DetailsActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to the action bar's Up/Home button
             case android.R.id.home:
-                supportFinishAfterTransition();
+//                supportFinishAfterTransition();
+                exitReveal();
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onBackPressed() {
+        exitReveal();
+    }
+
+    void enterReveal() {
+        // previously invisible view
+        final View myView = fab;
+
+        // get the center for the clipping circle
+        int cx = myView.getMeasuredWidth() / 2;
+        int cy = myView.getMeasuredHeight() / 2;
+
+        // get the final radius for the clipping circle
+        int finalRadius = Math.max(myView.getWidth(), myView.getHeight()) / 2;
+
+        // create the animator for this view (the start radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
+
+        // make the view visible and start the animation
+        myView.setVisibility(View.VISIBLE);
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                getWindow().getEnterTransition().removeListener(mEnterTransitionListener);
+            }
+        });
+        anim.start();
+    }
+
+    void exitReveal() {
+        // previously visible view
+        final View myView = fab;
+
+        // get the center for the clipping circle
+        int cx = myView.getMeasuredWidth() / 2;
+        int cy = myView.getMeasuredHeight() / 2;
+
+        // get the initial radius for the clipping circle
+        int initialRadius = myView.getWidth() / 2;
+
+        // create the animation (the final radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(myView, cx, cy, initialRadius, 0);
+
+        // make the view invisible when the animation is done
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                myView.setVisibility(View.INVISIBLE);
+                supportFinishAfterTransition();
+            }
+        });
+
+        // start the animation
+        anim.start();
+    }
 }
